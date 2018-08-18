1ac78b2a236501e2463d7ebe039497c2d2748b02
rusbey95 <rusikbey95@gmail.com>
2018-08-18 15:42:05 +0300

    2 lesson

--- js/main.js
+++ js/main.js
@@ -1,1 +1,14 @@
-alert("alert txt");
+//****** alert 1 lesson ******//
+// alert("alert txt");
+
+
+
+//****** code structure 2 lesson ******//
+alert("txt");
+[1, 2].forEach(alert);
+
+//comments one line
+/*
+ comments two line
+ comments two line
+*/ 
