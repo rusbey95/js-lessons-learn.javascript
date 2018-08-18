b13a2e85fe58c80b1c6556854f57eb0a19370529
rusbey95 <rusikbey95@gmail.com>
2018-08-18 23:50:53 +0300

    3 lesson variable

--- js/main.js
+++ js/main.js
@@ -4,11 +4,40 @@
 
 
 //****** code structure 2 lesson ******//
-alert("txt");
-[1, 2].forEach(alert);
+// alert("txt");
+// [1, 2].forEach(alert);
 
 //comments one line
 /*
  comments two line
  comments two line
 */ 
+
+
+
+
+//****** variable structure 3 lesson ******//
+// var i = 2;
+// console.log(i);
+// var y;
+// y = i;
+// console.log(y);
+
+//reserve variable: var; class; return; export ...
+
+// var x = 1;
+// var y = 2;
+// var z = 3;
+// console.log((x + y) * z);
+
+//constant variable: 
+// var COLOR_ORANGE = "#FF7F00";
+// var COLOR_WHITE = "#ffffff";
+// var COLOR_BLACK = "#000000";
+// console.log(COLOR_BLACK);
+
+//******************************HOMEWORK******************************//
+// var admin, name;
+// var name = "Vasiliy";
+// admin = name;
+// console.log(admin);
