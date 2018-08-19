3f915a58ea93ac0a5cce9e11b6f9fad59b660f8e
rusbey95 <rusikbey95@gmail.com>
2018-08-19 17:54:56 +0300

    5 lesson Basic Operators

--- js/main.js
+++ js/main.js
@@ -56,14 +56,65 @@
 
 // console.log(number, string, years, noName, date, user);
 
+//******************************HOMEWORK******************************//
+// console.log(
+// 	typeof 1, //number 
+// 	typeof "text", //string
+// 	typeof true, //boolean
+// 	typeof null, //object "error" this null
+// 	typeof undefined, // undefined
+// 	typeof {userName: "Kolya"},  //object
+// 	typeof function(){} //function error this object 
+// );
+
+
+
+
+//****** Basic Operators 5 lesson ******//
+// var a = 10;
+// var b = "2";
+// console.log(c = a + +b);
+// var z = "12";
+// console.log(y = (z - (a + b * c)));
+
+// var c = "text";
+// var y = 10;
+// console.log(z = c + y);
+// console.log(typeof z);
+
+// var i = 0;
+// console.log(i++ * 10);
+// console.log(++i * 10);
+
+// var x = 1;
+// console.log(--x);
+// console.log(x--);
+// console.log(x);
+// console.log(++x - 10 * 2 - 1); //-21
+// console.log(x);
+
+// var a = 1;
+// var b = 2;
+// var c = 3;
+// a *= 2;
+// b = c;
+// console.log(c);
+
+// for (a = 1, b = 3, c = a * b; a < 10; a++){}
 
 //******************************HOMEWORK******************************//
-console.log(
-	typeof 1, //number 
-	typeof "text", //string
-	typeof true, //boolean
-	typeof null, //object "error" this null
-	typeof undefined, // undefined
-	typeof {userName: "Kolya"},  //object
-	typeof function(){} //function error this object 
-);
+// var a = 1, b = 2, c, d;
+
+// c = ++a; console.log(c); //2
+// d = b++; console.log(d); //2
+
+// c = (2+ ++a); console.log(c); //5
+// d = (2+ b++); console.log(d); //5
+
+// console.log(a, b); //3,4
+
+//----------------------------------------
+
+// var a = 2;
+// var x = 1 + (a *= 2);
+// console.log(x);
